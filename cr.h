844e2d4269ef0fd0d09ba62b30496c0c6f509cb8
septag <sep.tagh@gmail.com>
2019-02-11 20:48:17 +0330

    minor changes

--- cr.h
+++ cr.h
@@ -55,7 +55,7 @@ While the guest (real application), would be like:
 
 ```c
 CR_EXPORT int cr_main(struct cr_plugin *ctx, enum cr_op operation) {
-    assert(ctx);
+    CR_ASSERT(ctx);
     switch (operation) {
         case CR_LOAD:   return on_load(...);
         case CR_UNLOAD: return on_unload(...);
@@ -251,7 +251,6 @@ You can define these macros before including cr.h in host (CR_HOST) to customize
 - `CR_ERROR`: logs debug messages to stderr. default (CR_DEBUG only): #define CR_ERROR(...) fprintf(stderr, __VA_ARGS__)
 - `CR_LOG`: logs debug messages. default (CR_DEBUG only): #define CR_LOG(...) fprintf(stdout, __VA_ARGS__)
 - `CR_TRACE`: prints function calls. default (CR_DEBUG only): #define CR_TRACE(...) fprintf(stdout, "CR_TRACE: %s\n", __FUNCTION__)
-- `CR_WINDOWS_UTF8_PATHS`: on windows platform, set this to zero to restrict the api to ascii file paths only (for better performance). (default: 1)
 
 ### FAQ / Troubleshooting
 
@@ -722,32 +721,6 @@ static void cr_del(const std::string& path) {
     CR_WINDOWS_ConvertPath(_path, path);
     DeleteFile(_path.c_str());
 }
-#else
-static time_t cr_last_write_time(const std::string &path) {
-    WIN32_FILE_ATTRIBUTE_DATA fad;
-    if (!GetFileAttributesExA(path.c_str(), GetFileExInfoStandard, &fad)) {
-        return -1;
-    }
-
-    LARGE_INTEGER time;
-    time.HighPart = fad.ftLastWriteTime.dwHighDateTime;
-    time.LowPart = fad.ftLastWriteTime.dwLowDateTime;
-
-    return static_cast<time_t>(time.QuadPart / 10000000 - 11644473600LL);
-}
-
-static bool cr_exists(const std::string &path) {
-    return GetFileAttributesA(path.c_str()) != INVALID_FILE_ATTRIBUTES;
-}
-
-static void cr_copy(const std::string &from, const std::string &to) {
-    CopyFileA(from.c_str(), to.c_str(), false);
-}
-
-static void cr_del(const std::string& path) {
-    DeleteFileA(path.c_str());
-}
-#endif // CR_WINDOWS_UTF8_PATHS
 
 // If using Microsoft Visual C/C++ compiler we need to do some workaround the
 // fact that the compiled binary has a fullpath to the PDB hardcoded inside
